f318d672fda5cdd98161edb8d307e9b15c952c0b
jfrfonseca <jfra.fonseca@gmail.com>
2015-03-07 01:48:58 -0300

    Atualização do teste do WebSocketThreadObject para um teste repetível

--- app/transceiver/implementation/tests/all/echoServer.py
+++ app/transceiver/implementation/tests/all/echoServer.py
@@ -4,18 +4,20 @@ from WebSocketThreadObject import WebSocketThreadObject  # @UnresolvedImport
 verificationInterval = 0.02
 
 print "setting up echo server"
-echoServer = WebSocketThreadObject(23019)
+echoServer = WebSocketThreadObject('', 23019)
 print "Echo server all set up!"
 
 print "getting messages:"
 message = ''
-while message == '':
-    time.sleep(verificationInterval)
-    message = echoServer.getMessagesBuffer()[0]
-print "we've got messages!"
-print "message: "+message
-print "sending echo back"
-echoServer.sendMessage(message)
-print "all done" 
+while 1:
+    while message == '':
+        time.sleep(verificationInterval)
+        message = echoServer.getMessagesBuffer()[0]
+    print "we've got messages!"
+    print "message: "+message
+    print "sending echo back"
+    echoServer.sendMessage(message)
+    print "all done"
+    echoServer.clearMessagesBuffer()
+    message = ''
 
-echoServer.printTest()
